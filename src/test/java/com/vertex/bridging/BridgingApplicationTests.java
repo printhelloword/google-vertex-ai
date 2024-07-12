f1e5aea534299c7582f2d8a1c3df1a832140b97d
printhelloword <slessudjono@gmail.com>
2024-07-12 10:44:17 +0700

    add default test class

---
+++ src/test/java/com/vertex/bridging/BridgingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vertex.bridging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BridgingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
