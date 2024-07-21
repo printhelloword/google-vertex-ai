8289b4581424aa98291dc0887b82bc6b74310833
Sles <93713343+printhelloword@users.noreply.github.com>
2024-07-21 20:05:57 +0700

    add cofigs (#1)

--- src/main/java/com/vertex/bridging/config/GoogleConsoleConfig.java
+++ src/main/java/com/vertex/bridging/config/GoogleConsoleConfig.java
@@ -8,7 +8,11 @@ import org.springframework.stereotype.Component;
 @Component
 @ConfigurationProperties("google.console")
 public class GoogleConsoleConfig {
+    private String apiKey;
     private String bucketName;
     private String projectId;
     private String dataStorageId;
+    private String agentId;
+    private String regionId;
+    private String cred;
 }
