3b2a449d6edd0b170e6d5caa114953b14ee47e01
Sles <93713343+printhelloword@users.noreply.github.com>
2024-07-22 03:28:23 +0700

    Feature/chat agent service (#4)
    * add cofigs
    
    * remove unused lines
    
    * add produce json content type
    
    * implement google api client service

--- src/main/java/com/vertex/bridging/controller/ChatAgentController.java
+++ src/main/java/com/vertex/bridging/controller/ChatAgentController.java
@@ -1,19 +1,14 @@
 package com.vertex.bridging.controller;
 
-import com.google.auth.oauth2.GoogleCredentials;
 import com.vertex.bridging.service.ChatAgentService;
-import com.vertex.bridging.service.DialogFlowService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.FileInputStream;
 
 @Log4j2
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/chat-agent")
+@RequestMapping(value = "/chat-agent", produces = "application/json")
 public class ChatAgentController {
 
     private final ChatAgentService chatAgentService;
